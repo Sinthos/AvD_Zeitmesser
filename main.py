d47dc7d7041b6a12be6c2469bf0e0b8a50461f89
maximilian <info@maxtempel.de>
2024-03-29 11:30:19 +0100

    0.4 Ohne API Mit Reset Mit Excel Export Mit Namen Mit Strafzeit

--- main.py
+++ main.py
@@ -14,14 +14,15 @@ class LightBarrierApp:
         self.timer_queue = deque()
 
         # Tabelle erstellen
-        self.tree = ttk.Treeview(root, columns=('Messungsname', 'Startzeit', 'Endzeit', 'Dauer'), show='headings')
+        self.tree = ttk.Treeview(root, columns=('Messungsname', 'Startzeit', 'Endzeit', 'Dauer', 'Strafzeit'), show='headings')
         self.tree.heading('Messungsname', text='Messungsname')
         self.tree.heading('Startzeit', text='Startzeit')
         self.tree.heading('Endzeit', text='Endzeit')
         self.tree.heading('Dauer', text='Dauer (s)')
+        self.tree.heading('Strafzeit', text='Strafzeit')
         self.tree.pack(expand=True, fill='both')
 
-        # Doppelklick-Event für das Editieren von Messungsnamen
+        # Doppelklick-Event für das Editieren von Messungsnamen und Strafzeiten
         self.tree.bind("<Double-1>", self.on_item_double_click)
 
         # Knöpfe für Start- und Endzeit
@@ -43,7 +44,7 @@ class LightBarrierApp:
 
     def set_start_time(self):
         start_time = datetime.now()
-        item_id = self.tree.insert('', 'end', values=('Unbenannt', start_time, 'Läuft...', '0.00 s'))
+        item_id = self.tree.insert('', 'end', values=('Unbenannt', start_time, 'Läuft...', '0.00 s', '0'))
         self.timer_items[item_id] = (start_time, 'Läuft...')
         self.timer_queue.append(item_id)
 
@@ -54,29 +55,34 @@ class LightBarrierApp:
                 end_time = datetime.now()
                 start_time, _ = self.timer_items[item_id]
                 duration = (end_time - start_time).total_seconds()
-                self.tree.item(item_id, values=('Unbenannt', start_time, end_time, '{:.2f} s'.format(duration)))
+                self.tree.item(item_id, values=('Unbenannt', start_time, end_time, '{:.2f} s'.format(duration), '0'))
                 del self.timer_items[item_id]
 
     def on_item_double_click(self, event):
+        # Spalten-ID zu Index-Mapping
+        column_id_to_index = {'#1': 0, '#2': 1, '#3': 2, '#4': 3, '#5': 4}
+
         column = self.tree.identify_column(event.x)
-        if column == '#1':
+        if column in column_id_to_index:
             item_id = self.tree.identify_row(event.y)
             entry = tk.Entry(self.tree)
-            entry.insert(0, self.tree.item(item_id, 'values')[0])
-            entry.bind("<Return>", lambda e: self.update_item_name(entry, item_id))
+            entry.insert(0, self.tree.item(item_id, 'values')[column_id_to_index[column]])
+            entry.bind("<Return>", lambda e: self.update_item(entry, item_id, column_id_to_index[column]))
             entry.bind("<FocusOut>", lambda e: entry.destroy())
             entry.place(x=event.x, y=event.y)
             entry.focus_set()
 
-    def update_item_name(self, entry, item_id):
-        self.tree.item(item_id, values=(entry.get(),) + self.tree.item(item_id, 'values')[1:])
+    def update_item(self, entry, item_id, column_index):
+        values = list(self.tree.item(item_id, 'values'))
+        values[column_index] = entry.get()
+        self.tree.item(item_id, values=values)
         entry.destroy()
 
     def update_timer(self):
         now = datetime.now()
         for item_id, (start_time, _) in list(self.timer_items.items()):
             duration = (now - start_time).total_seconds()
-            self.tree.item(item_id, values=('Unbenannt', start_time, 'Läuft...', '{:.2f} s'.format(duration)))
+            self.tree.item(item_id, values=('Unbenannt', start_time, 'Läuft...', '{:.2f} s'.format(duration), '0'))
         self.root.after(100, self.update_timer)
 
     def reset_timers(self):
@@ -97,14 +103,16 @@ class LightBarrierApp:
             worksheet.write('B1', 'Startzeit')
             worksheet.write('C1', 'Endzeit')
             worksheet.write('D1', 'Dauer (s)')
+            worksheet.write('E1', 'Strafzeit')
 
             # Schreibe die Daten
             for i, item_id in enumerate(self.tree.get_children(), start=1):
-                messungsname, start_time, end_time, duration = self.tree.item(item_id)['values']
+                messungsname, start_time, end_time, duration, strafzeit = self.tree.item(item_id)['values']
                 worksheet.write(i, 0, messungsname)
                 worksheet.write(i, 1, str(start_time))
                 worksheet.write(i, 2, str(end_time))
-                worksheet.write(i, 3, str(duration))
+                worksheet.write(i, 3, duration)
+                worksheet.write(i, 4, strafzeit)
 
             workbook.close()
 
